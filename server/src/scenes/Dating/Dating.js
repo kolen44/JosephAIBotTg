5646c35e8c6a914ff98c26cb68695b61b6a2db6e
kolen44 <malavov70@gmail.com>
2024-02-25 17:04:05 +0300

    fix: Dialogue scenes linked Dialogue scenes have been linked, chat prompts have been improved. Clarifying questions were also added and the telegraf dialogue scene became larger. In fact, now this is a minimal version of the application that will be expanded.

--- server/src/scenes/Dating/Dating.js
+++ server/src/scenes/Dating/Dating.js
@@ -1,11 +1,12 @@
 const { Scenes } = require('telegraf')
+const ChatGPT = require('../../chatgpt')
 
 function greetingScene() {
 	const greeting = new Scenes.WizardScene(
 		'CONTACT_DATA_WIZARD_SCENE_ID', // first argument is Scene_ID, same as for BaseScene
 		ctx => {
 			ctx.reply('Давайте для начала познакомимся . Как Вас зовут?')
-			ctx.wizard.state.contactData = {}
+			ctx.wizard.state.firstDialog = {}
 			return ctx.wizard.next()
 		},
 		ctx => {
@@ -14,16 +15,35 @@ function greetingScene() {
 				ctx.reply(`Не обманывайте . Введите Ваше настоящее имя`)
 				return
 			}
-			ctx.wizard.state.contactData.fio = ctx.message.text
+			ctx.wizard.state.firstDialog.fio = ctx.message.text
 			ctx.reply(`Какой ваш вес ${ctx.message.text}?`)
 			return ctx.wizard.next()
 		},
-		async ctx => {
-			ctx.wizard.state.contactData.weight = ctx.message.text.replace('кг', '')
+		ctx => {
+			ctx.wizard.state.firstDialog.weight = ctx.message.text
 			ctx.reply(
-				'Введите вашу спортивную цель и можем приступать к созданию плана...'
+				'Для начала, чтобы создать эффективный план тренировок и план питания, мне нужно знать больше информации о тебе: твоем возрасте,рост,пол, текущей физической подготовке, наличии травм или заболеваний, режиме дня и сколько времени в неделю ты готов уделять тренировкам. Постарайся написать это максимально подробно для генерации наилучшего плана тренировок!'
 			)
 			//await mySendContactDataMomentBeforeErase(ctx.wizard.state.contactData)
+			return ctx.wizard.next()
+		},
+		ctx => {
+			ctx.wizard.state.firstDialog.someInfo = ctx.message.text
+			ctx.reply(
+				'Также важно определить конкретную цель: хотите ли вы похудеть, набрать мышечную массу, улучшить выносливость или силу. Поэтому напиши свою цель, так же можешь указать время за которое хочишь ее достичь🤠'
+			)
+			return ctx.wizard.next()
+		},
+		async ctx => {
+			ctx.reply('Нейросеть генерирует ответ...🔍')
+			ctx.wizard.state.firstDialog.goal = ctx.message.text
+			const response = await ChatGPT(
+				ctx.wizard.state.firstDialog.fio,
+				ctx.wizard.state.firstDialog.wight,
+				ctx.wizard.state.firstDialog.someInfo,
+				ctx.wizard.state.firstDialog.goal
+			)
+			await ctx.reply(response)
 			return ctx.scene.leave()
 		}
 	)
